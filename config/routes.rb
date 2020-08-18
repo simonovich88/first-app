984f3dd3dcdf2a290a589de7c57218bec344f980
Simon <acarigua88@mail.com>
2020-08-17 21:35:14 -0400

    adding page one

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/one'
   get 'pages/two'
+  get 'pages/three'
+  root 'pages#three'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
